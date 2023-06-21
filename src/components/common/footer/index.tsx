f7077b8cd49f27d49187bf38cba1a5b27128ec85
Houston Barros <capar.2022113TADS0170@aluno.ifpi.edu.br>
2023-06-21 14:53:20 -0300

    Adicionar Status do Sistema

--- src/components/common/footer/index.tsx
+++ src/components/common/footer/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
 /* eslint-disable @next/next/no-img-element */
 import { Container } from 'reactstrap';
 import styles from './styles.module.scss';
@@ -6,7 +7,7 @@ const Footer = function () {
     return (
         <>
             <Container className={styles.footer}>
-                <p className={styles.text}><a href='google.com' className={styles.footerLink}>Status do sistema</a> | <a href='google.com' className={styles.footerLink}>Política de privacidade</a> | <a href='google.com' className={styles.footerLink}>Termos e condições</a></p>
+                <p className={styles.text}><a href='/status' className={styles.footerLink}>Status do sistema</a> | <a href='google.com' className={styles.footerLink}>Política de privacidade</a> | <a href='google.com' className={styles.footerLink}>Termos e condições</a></p>
                 <p className={styles.text}>Copyright © 2023 Agendei. Todos os direitos reservados.</p>
             </Container>
         </>
